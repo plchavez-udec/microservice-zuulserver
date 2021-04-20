f4c6ff8b690fc2599d591ec77048172ba5190c99
Pedro Luis Chavez Castro <plchavez@ucundinamarca.edu.co>
2021-04-20 18:02:16 -0500

    gegseg

--- src/main/java/co/edu/ierdminayticha/sgd/zuulserver/oauth/ResourceServerConfig.java
+++ src/main/java/co/edu/ierdminayticha/sgd/zuulserver/oauth/ResourceServerConfig.java
@@ -1,17 +1,34 @@
 package co.edu.ierdminayticha.sgd.zuulserver.oauth;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+@RefreshScope
 @Configuration
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	
+	@Value("${config.security.oauth.jwt.key}")
+	private String jwtKey;
+	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore());
@@ -21,9 +38,34 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/api/security/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/api/folders/logical-folder/v1/logical-folder/").hasRole("ADMIN")
-				.anyRequest().authenticated();
+				.anyRequest().authenticated()
+				.and().cors().configurationSource(corsConfigurationSource());// COnsigurar quien puede cpnsumir el servicio
 
 		;
+	}
+	
+	// COnsigurar quien puede cpnsumir el servicio
+	@Bean
+	public  CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*")); // Define que todsos pueden acceder
+		corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource basedCorsConfigurationSource= new UrlBasedCorsConfigurationSource();
+		basedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		
+		return basedCorsConfigurationSource;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 	@Bean
@@ -37,7 +79,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
 		// Codigo secreto para generar y validar el token
-		accessTokenConverter.setSigningKey("daniAmor");
+		accessTokenConverter.setSigningKey(jwtKey);
 		return accessTokenConverter;
 	}
 
